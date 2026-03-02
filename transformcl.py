60fd6c7bb3da75e29e87f00b4406fc4dbd395347
Connor Aird <c.aird@ucl.ac.uk>
2026-03-02 19:12:22 +0000

    Fix var to be array api compatible by using float64 for __truediv__ (#6)

--- transformcl.py
+++ transformcl.py
@@ -138,7 +138,9 @@ def var(cl):
 
     """
     xp = array_namespace(cl)
-    ell = xp.arange(cl.shape[-1])
+    # ell cannot be an integer here as, within the array api
+    # only floating-point dtypes are allowed in __truediv__
+    ell = xp.arange(cl.shape[-1], dtype=xp.float64)
     return xp.sum((2 * ell + 1) / (4 * xp.pi) * cl, axis=-1)
 
 
